e03744f14801d0df2303dcf4ce39199251968a08
e.tuchs <e.tuchs@gmail.com>
2016-12-01 15:01:06 +0800

    Update

--- python/deal/post.py
+++ python/deal/post.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
-import json
 import urllib2
 
 DEF_HEAD = {'Content-Type': 'application/json; charset=utf-8'}
 
 
 def post_data(req_url, req_data, headers=DEF_HEAD):
-    req = urllib2.Request(url=req_url, data=json.dumps(req_data), headers=headers)
+    req = urllib2.Request(url=req_url, data=req_data, headers=headers)
     response = urllib2.urlopen(req)
     msg_resp = response.read()
     return msg_resp
